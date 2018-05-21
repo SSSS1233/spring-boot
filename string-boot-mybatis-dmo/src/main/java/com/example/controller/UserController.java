c3dbc80105cd3ab3944b8b07bc1771d6df5a879d
SSSS1233 <sb904428391@gmail.com>
2018-05-21 16:32:19 +0800

    sping-boot-demo

--- string-boot-mybatis-dmo/src/main/java/com/example/controller/UserController.java
+++ string-boot-mybatis-dmo/src/main/java/com/example/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.po.User;
 import com.example.po.UserExpand;
 import com.example.service.UserServiceImpl;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +25,11 @@ public class UserController {
     UserServiceImpl userService;
 
     @RequestMapping("/index")
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    /* @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })*/
     public String index(Model m, HttpSession session) {
         m.addAttribute("demo","hello");
         session.setAttribute("name","宿冰");
